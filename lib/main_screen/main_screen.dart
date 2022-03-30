fc8189624d63d807bd40bc7c909a86acf9468f97
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-30 03:26:45 +0100

    put the api 1

--- lib/main_screen/main_screen.dart
+++ lib/main_screen/main_screen.dart
@@ -11,6 +11,9 @@ class univs extends StatefulWidget {
 }
 
 class _univsState extends State<univs> {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
   @override
   void initState() {
     getdata();
@@ -37,6 +40,19 @@ class _univsState extends State<univs> {
             subtitle: Text("yes im mustapha"),
             trailing: Icon(Icons.person),
           ),
+          ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          )
         ],
       )),
     );
