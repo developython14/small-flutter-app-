1253009f3a93cdc25fa1e42227b3a509cf7708d4
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-30 02:19:31 +0100

    put the api

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mustapha/main_screen/main_screen.dart';
 
 void main() {
-  runApp(MaterialApp(title: 'mustapha belkassem ', initialRoute: '/', routes: {
+  runApp(MaterialApp(title: 'mustapha developper ', initialRoute: '/', routes: {
     '/': (context) => univs(),
   }));
 }
