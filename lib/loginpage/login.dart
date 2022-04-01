b013321c8cdd7638fd687c1103abf9bccdd41da7
djmel1955@gmail.com <djmel1955@gmail.com>
2022-04-01 18:04:03 +0100

    trying every  widgets

--- lib/loginpage/login.dart
+++ lib/loginpage/login.dart
@@ -9,6 +9,8 @@ class login extends StatefulWidget {
 
 class _loginState extends State<login> {
   final _formKey = GlobalKey<FormState>();
+  String name = 'mus';
+  var password = 'free';
 
   @override
   Widget build(BuildContext context) {
@@ -24,68 +26,31 @@ class _loginState extends State<login> {
           SizedBox(height: 25),
           Text('Login',
               style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
-          SizedBox(height: 25),
-          TextField(
-            decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(
-                        color: Color.fromARGB(255, 230, 230, 230), width: 1)),
-                hintText: "email or username",
-                icon: Icon(
-                  Icons.email,
-                  color: Colors.black,
-                )),
-          ),
-          SizedBox(height: 25),
-          Container(
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: "password",
-                  suffixIcon:
-                      IconButton(onPressed: () {}, icon: Icon(Icons.password)),
-                  icon: Icon(
-                    Icons.lock,
-                    color: Colors.black,
-                  )),
-            ),
-          ),
-          SizedBox(height: 25),
-          RaisedButton(
-            onPressed: () {},
-            child: Text("login"),
-            color: Colors.blue,
-          ),
-          SizedBox(height: 25),
-          Text("or login using "),
-          SizedBox(height: 25),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            GFIconButton(
-              onPressed: () {},
-              icon: Icon(Icons.facebook),
-            ),
-            SizedBox(width: 25),
-            GFIconButton(
-              onPressed: () {},
-              icon: Icon(Icons.web),
-            ),
-            SizedBox(width: 25),
-            GFIconButton(
-              onPressed: () {},
-              icon: Icon(Icons.mail),
-            ),
-          ]),
-          SizedBox(
-            width: 200,
-            child: TextField(
-              autofocus: true,
-              decoration: InputDecoration(hintText: "write test text"),
-              onChanged: (text) {
-                print("raho katteb $text");
-              },
-            ),
-          )
+          Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(hintText: 'name'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(hintText: 'pass'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ],
+              ))
         ]),
       ),
     );
