b6dae49b5ae025b290e6bddb1cdc0a14ae383522
djmel1955@gmail.com <djmel1955@gmail.com>
2022-04-06 16:00:38 +0100

    trying every  widgets

--- lib/signup_pages/signup.dart
+++ lib/signup_pages/signup.dart
@@ -10,8 +10,12 @@ class signup extends StatefulWidget {
 
 class _signupState extends State<signup> {
   final _formKey = GlobalKey<FormState>();
+  bool is_pass = true;
   int groupValue = 0;
-  final my = ["mustapha", "belkassem", "allo", "paris"];
+  final list_countries = ["mustapha", "belkassem", "allo", "paris"];
+  final list_faculties = ["mustapha", "belkassem", "allo", "paris"];
+  final list_spicialities = ["mustapha", "belkassem", "allo", "paris"];
+  final list_levels = ["mustapha", "belkassem", "allo", "paris"];
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +78,9 @@ class _signupState extends State<signup> {
                   GFRadioListTile(
                     titleText: 'Arthur Shelby',
                     subTitleText: 'By order of the peaky blinders',
-                   
+                   GFAvatar(
+    backgroundImage: AssetImage('Assets image here'),
+  ),
                     size: 25,
                     activeBorderColor: Colors.green,
                     focusColor: Colors.green,
@@ -93,21 +99,27 @@ class _signupState extends State<signup> {
                     children: [
                       Text("Select country"),
                       DropdownButton(
-                          items: my.map(buildmen).toList(),
+                          items: list_countries.map(buildmen).toList(),
                           onChanged: (text) {})
                     ],
                   ),
                   SizedBox(height: 20),
                   Row(
-                    children: [Text("Select Faculty")],
+                    children: [Text("Select Faculty"), DropdownButton(
+                          items: list_faculties.map(buildmen).toList(),
+                          onChanged: (text) {})],
                   ),
                   SizedBox(height: 20),
                   Row(
-                    children: [Text("Select Speciality ")],
+                    children: [Text("Select Speciality "),DropdownButton(
+                          items: list_spicialities.map(buildmen).toList(),
+                          onChanged: (text) {})],
                   ),
                   SizedBox(height: 20),
                   Row(
-                    children: [Text("Select Level")],
+                    children: [Text("Select Level")],DropdownButton(
+                          items: list_levels.map(buildmen).toList(),
+                          onChanged: (text) {}),
                   ),
                   SizedBox(height: 20),
                   TextFormField(
@@ -122,6 +134,14 @@ class _signupState extends State<signup> {
                     decoration: InputDecoration(
                         hintText: 'Password',
                         prefixIcon: Icon(Icons.email),
+                         suffixIcon: IconButton(
+                            icon: Icon(Icons.password),
+                            onPressed: () {
+                              setState(() {
+                                is_pass = !is_pass;
+                              });
+                            },
+                          ),
                         border: OutlineInputBorder()),
                     onSaved: (text) {},
                   ),
@@ -130,6 +150,14 @@ class _signupState extends State<signup> {
                     decoration: InputDecoration(
                         hintText: 'Repeat Password',
                         prefixIcon: Icon(Icons.email),
+                         suffixIcon: IconButton(
+                            icon: Icon(Icons.password),
+                            onPressed: () {
+                              setState(() {
+                                is_pass = !is_pass;
+                              });
+                            },
+                          ),
                         border: OutlineInputBorder()),
                     onSaved: (text) {},
                   ),
@@ -144,8 +172,9 @@ class _signupState extends State<signup> {
                   SizedBox(height: 20),
                   GFButton(
                     onPressed: () {},
-                    text: "primary",
+                    text: "Signup",
                     shape: GFButtonShape.pills,
+                      fullWidthButton: true,
                   ),
                 ],
               )),
